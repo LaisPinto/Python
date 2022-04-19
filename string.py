a2fd8680768e1ac4faa5c0ebc15b36e8ea4459b3
Lais Pinto <laisp19@hotmail.com>
2022-04-18 23:20:31 -0300

    v1

---
+++ string.py
@@ -0,0 +1,7 @@
+nome1 = "lais"
+nome2 = 'pinto'
+nome_completo = nome1 + " " + nome2
+nome_completo = nome_completo.title()
+
+print(nome_completo.title())
+print("ola " + nome_completo + ", tudo bem?")
---
+++ variaveis.py
@@ -0,0 +1,10 @@
+num = 10
+num2 = 10.5
+
+texto = True
+
+print(type(texto))
+print(num)
+
+message = "ola, td bem?"
+print(message)
